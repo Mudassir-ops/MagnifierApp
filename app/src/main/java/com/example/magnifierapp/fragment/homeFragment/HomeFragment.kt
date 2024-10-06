462ae49a4999794f01f41d71f5789cebb97de217
Mudasir Ops <dreame488@gmail.com>
2024-10-06 17:26:41 +0500

    adjsut scalling and rotation of cameraview

--- app/src/main/java/com/example/magnifierapp/fragment/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/magnifierapp/fragment/homeFragment/HomeFragment.kt
@@ -2,11 +2,11 @@ package com.example.magnifierapp.fragment.homeFragment
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Surface
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
@@ -25,13 +25,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.magnifierapp.databinding.FragmentHomeBinding
 import com.example.magnifierapp.fragment.camerax.YuvToRgbConverter
 import com.example.utils.gone
-import com.example.utils.toast
 import com.example.utils.visible
-import com.google.android.material.internal.FlowLayout
 import com.google.android.material.slider.Slider
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.cyberagent.android.gpuimage.GPUImage
-import jp.co.cyberagent.android.gpuimage.GPUImageView
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageBrightnessFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageColorInvertFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageContrastFilter
@@ -40,6 +37,7 @@ import jp.co.cyberagent.android.gpuimage.filter.GPUImageGlassSphereFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageGrayscaleFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageSketchFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageSolarizeFilter
+import jp.co.cyberagent.android.gpuimage.util.Rotation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -55,61 +53,82 @@ class HomeFragment : Fragment() {
     private val viewModel: HomeViewModel by viewModels()
     private var adapter: FilterAdapter? = null
     private var isFilterClicked = true
-    private var zoomJob: Job? = null
-
-
-    companion object {
-        const val REQUEST_CODE_PERMISSIONS = 10
-    }
-
-    private lateinit var gpuImageView: GPUImageView
     private var cameraProvider: ProcessCameraProvider? = null
     private val executor: ExecutorService = Executors.newSingleThreadExecutor()
     private lateinit var converter: YuvToRgbConverter
     private var bitmap: Bitmap? = null
-    private lateinit var zoomSlider: Slider
     private lateinit var cameraControl: CameraControl
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         adapter = FilterAdapter(filterList = listOf(), onItemClick = { itemAndPosition ->
             val (filterDataModel, position) = itemAndPosition
             println("Item clicked: ${filterDataModel.filterImage} at position $position")
-           // activity?.toast("position $position")
-            when(position){
-                0->{
-                   binding?.cameraPreview?.filter =  GPUImageFilter()
+            // activity?.toast("position $position")
+            when (position) {
+                0 -> {
+                    binding?.cameraPreview?.filter = GPUImageFilter()
                 }
-                1->{
-                    binding?.cameraPreview?.filter =  GPUImageSketchFilter()
+
+                1 -> {
+                    binding?.cameraPreview?.filter = GPUImageSketchFilter()
                 }
-                2->{
-                    binding?.cameraPreview?.filter =   GPUImageColorInvertFilter()
+
+                2 -> {
+                    binding?.cameraPreview?.filter = GPUImageColorInvertFilter()
                 }
-                3->{
-                    binding?.cameraPreview?.filter =   GPUImageSolarizeFilter()
+
+                3 -> {
+                    binding?.cameraPreview?.filter = GPUImageSolarizeFilter()
                 }
-                4->{
-                    binding?.cameraPreview?.filter =   GPUImageGrayscaleFilter()
+
+                4 -> {
+                    binding?.cameraPreview?.filter = GPUImageGrayscaleFilter()
                 }
-                5->{
-                    binding?.cameraPreview?.filter =    GPUImageBrightnessFilter(0.8f)
+
+                5 -> {
+                    binding?.cameraPreview?.filter = GPUImageBrightnessFilter(0.8f)
                 }
-                6->{
-                    binding?.cameraPreview?.filter =   GPUImageContrastFilter(2f)
+
+                6 -> {
+                    binding?.cameraPreview?.filter = GPUImageContrastFilter(2f)
                 }
-                7->{
-                    binding?.cameraPreview?.filter =   GPUImageGlassSphereFilter()
+
+                7 -> {
+                    binding?.cameraPreview?.filter = GPUImageGlassSphereFilter()
                 }
             }
 
 
         })
 
-        converter = YuvToRgbConverter(context?:return)
+        converter = YuvToRgbConverter(context ?: return)
+    }
+
+    private fun getRotation(orientation: Int): Rotation {
+        return when (orientation) {
+            90 -> Rotation.ROTATION_90
+            180 -> Rotation.ROTATION_180
+            270 -> Rotation.ROTATION_270
+            else -> Rotation.NORMAL
+        }
+    }
+
+    fun getCameraOrientation(): Int {
+        val degrees = when (activity?.windowManager?.defaultDisplay?.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> 0
+        }
+        return (90 - degrees) % 360
+
+//        return if (false) {
+//            (90 + degrees) % 360
+//        } else { // back-facing
+//            (90 - degrees) % 360
+//        }
     }
 
     override fun onCreateView(
@@ -134,11 +153,6 @@ class HomeFragment : Fragment() {
         if (binding?.filterRecyclerView?.adapter == null) {
             binding?.filterRecyclerView?.adapter = adapter
         }
-
-        gpuImageView= binding?.cameraPreview?:return
-       // gpuImageView.rotation = 270F
-      //  gpuImageView.setScaleType(GPUImage.ScaleType.CENTER_CROP)
-
         clickListener()
         observerViewModel()
         cameraXSetup()
@@ -185,28 +199,26 @@ class HomeFragment : Fragment() {
             }
 
             icFilter.setOnClickListener {
-                if (isFilterClicked){
+                if (isFilterClicked) {
                     groupRecyclerView.visible()
                     verticalSeekbar.gone()
-                }else{
+                } else {
                     groupRecyclerView.gone()
                     verticalSeekbar.visible()
                 }
                 isFilterClicked = !isFilterClicked
             }
-
-
-
             binding?.verticalSeekbar?.apply {
                 max = 100
                 setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                        zoomJob?.cancel()
-                        zoomJob = CoroutineScope(Dispatchers.Main).launch {
-                            delay(10)
-                            val zoomLevel = progress / 100f
-                            cameraControl.setLinearZoom(zoomLevel)
-                        }
+                    override fun onProgressChanged(
+                        seekBar: SeekBar,
+                        progress: Int,
+                        fromUser: Boolean
+                    ) {
+                        val zoomValue = progress / 100.0f
+                        Log.e("ZoomValue", "onProgressChanged: $progress -> Zoom: $zoomValue")
+                        cameraControl.setLinearZoom(zoomValue)
                     }
 
                     override fun onStartTrackingTouch(seekBar: SeekBar) {}
@@ -240,39 +252,35 @@ class HomeFragment : Fragment() {
         builder.create().show()
     }
 
-    private fun cameraXSetup(){
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context?:return)
+    private fun cameraXSetup() {
+        binding?.cameraPreview?.setRotation(getRotation(getCameraOrientation()))
+        binding?.cameraPreview?.setScaleType(GPUImage.ScaleType.CENTER_CROP)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context ?: return)
         cameraProviderFuture.addListener({
             cameraProvider = cameraProviderFuture.get()
             startCameraIfReady()
-        }, ContextCompat.getMainExecutor(context?:return))
-
-//        zoomSlider.addOnChangeListener { _, value, _ ->
-//            cameraControl.setLinearZoom(value)
-//        }
+        }, ContextCompat.getMainExecutor(context ?: return))
     }
 
     @OptIn(ExperimentalGetImage::class)
     @SuppressLint("UnsafeExperimentalUsageError")
     private fun startCameraIfReady() {
-
-        // Set up image analysis
         val imageAnalysis = ImageAnalysis.Builder()
             .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
             .build()
-
         imageAnalysis.setAnalyzer(executor) { imageProxy ->
             val bitmap = allocateBitmapIfNecessary(imageProxy.width, imageProxy.height)
             imageProxy.image?.let { converter.yuvToRgb(it, bitmap) }
             imageProxy.close()
-            gpuImageView.post {
-                gpuImageView.setImage(bitmap)
+            binding?.cameraPreview?.apply {
+                post {
+                    setImage(bitmap)
+                }
             }
         }
-
         val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
         val camera = cameraProvider?.bindToLifecycle(this, cameraSelector, imageAnalysis)
-        cameraControl = camera?.cameraControl?:return
+        cameraControl = camera?.cameraControl ?: return
     }
 
     private fun allocateBitmapIfNecessary(width: Int, height: Int): Bitmap {
@@ -281,8 +289,6 @@ class HomeFragment : Fragment() {
         }
         return bitmap!!
     }
-
-
 
 
 }
